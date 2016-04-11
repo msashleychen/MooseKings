dc09d6bdb1afe8ade54e6fc44fff6725f48de088
foreverTuesday <scheffm@simmons.edu>
2016-04-11 09:20:34 -0400

    Added files via upload

--- MooseGameModel.java
+++ MooseGameModel.java
@@ -1,17 +1,80 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;  
+import java.awt.event.*;
+import java.lang.*;
+  
 
 
-abstract class GameModel{
+class MooseGameModel extends GameModel{
+   private ImageIcon [] images = new ImageIcon[16];
+   private int [] turn = {-1,-1};
+   private int attempts = 0;
+   private int matches = 0;
 
-   abstract void takeTurn(int t);           
-   abstract boolean gameOverStatus();
-   abstract int matchStatus();
-   abstract ImageIcon get(int i);  
-   abstract int getRows();
-   abstract int getCols(); 
-   abstract void display();      
-   abstract String reportWinner();   
+   MooseGameModel(){
+      for(int i=0;i<images.length;i++){
+         images[i] = new ImageIcon("moose.jpg");  
+      }
+   }//moosegamemodel
    
-}// class
+
+   void takeTurn(int clicked){
+      if(turn[0]==-1){
+         turn[0]=clicked;
+      }//if
+      else{
+         turn[1]=clicked;
+      }//else
+   }//check
+
+   int matchStatus(){
+      int status = 0;
+      if(turn[1]==-1){
+         status = 1;
+      }//if
+      else{
+         if(Math.abs(turn[0]-turn[1])==8){
+            status = 3;
+            matches = matches +1;
+         }//if
+         else{
+            status = 2;
+         }//else
+         turn[0] = -1;
+         turn[1] = -1;
+         attempts = attempts +1;
+      }//else
+   
+   //if so, check if they are the same
+   //return true if same, false if different
+   //reset check funtion if they are the same
+   // 3=match   2=not match   1=only one flipped
+      return status;}
+
+
+
+
+
+   boolean gameOverStatus(){
+   boolean over = false;
+   if(matches == 8){
+   over = true;
+   }//if
+      return over;};
+   
+   ImageIcon get(int i){
+     return(images[i]);}
+   
+   
+   int getRows(){
+      return 4;}
+   int getCols(){
+      return 4;} 
+   void display(){}     
+   String reportWinner(){
+      return "";}
+
+
+
+
+}
